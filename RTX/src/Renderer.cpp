d2e1e2b3c84f742d8bf10b396e629d89af344f7b
DevStuffzz <cb050129@gmail.com>
2022-09-22 17:38:03 -0400

    Camera System and Basic Renderer

--- RTX/src/Renderer.cpp
+++ RTX/src/Renderer.cpp
@@ -1,29 +1,34 @@
 #include "Renderer.h"
 
-void Renderer::Render()
-{
-	for (uint32_t y = 0; y < m_FinalImage->GetHeight(); y++) {
+#include "Walnut/Random.h"
 
-		for (uint32_t x = 0; x < m_FinalImage->GetWidth(); x++) {
-			glm::vec2 coord = { (float)x / (float)m_FinalImage->GetWidth(), (float)y / (float)m_FinalImage->GetHeight() };
-			coord = coord * 2.0f - 1.0f; 
-			
-			PerPixel(coord);
+namespace Utils {
 
-			m_ImageData[x + y * m_FinalImage->GetWidth()] = PerPixel(coord);
-		}
+	static uint32_t ConvertToRGBA(const glm::vec4& color)
+	{
+		uint8_t r = (uint8_t)(color.r * 255.0f);
+		uint8_t g = (uint8_t)(color.g * 255.0f);
+		uint8_t b = (uint8_t)(color.b * 255.0f);
+		uint8_t a = (uint8_t)(color.a * 255.0f);
+
+		uint32_t result = (a << 24) | (b << 16) | (g << 8) | r;
+		return result;
 	}
-	m_FinalImage->SetData(m_ImageData);
+
 }
 
 void Renderer::OnResize(uint32_t width, uint32_t height)
 {
-	if (m_FinalImage) {
-		if (m_FinalImage->GetWidth() == width && m_FinalImage->GetHeight() == height) return;
+	if (m_FinalImage)
+	{
+		// No resize necessary
+		if (m_FinalImage->GetWidth() == width && m_FinalImage->GetHeight() == height)
+			return;
 
 		m_FinalImage->Resize(width, height);
 	}
-	else {
+	else
+	{
 		m_FinalImage = std::make_shared<Walnut::Image>(width, height, Walnut::ImageFormat::RGBA);
 	}
 
@@ -31,56 +36,63 @@ void Renderer::OnResize(uint32_t width, uint32_t height)
 	m_ImageData = new uint32_t[width * height];
 }
 
-uint32_t Renderer::PerPixel(glm::vec2 coord)
+void Renderer::Render(const Camera& camera)
 {
-	uint8_t r = (uint8_t)(coord.x * 255.0f);
-	uint8_t g = (uint8_t)(coord.y * 255.0f);
+	Ray ray;
+	ray.Origin = camera.GetPosition();
 
-	glm::vec3 lightDirection = glm::vec3{-1, -1, 1};
-	lightDirection = glm::normalize(lightDirection);
+	for (uint32_t y = 0; y < m_FinalImage->GetHeight(); y++)
+	{
+		for (uint32_t x = 0; x < m_FinalImage->GetWidth(); x++)
+		{
+			ray.Direction = camera.GetRayDirections()[x + y * m_FinalImage->GetWidth()];
+			glm::vec4 color = TraceRay(ray);
+			color = glm::clamp(color, glm::vec4(0.0f), glm::vec4(1.0f));
+			m_ImageData[x + y * m_FinalImage->GetWidth()] = Utils::ConvertToRGBA(color);
+		}
+	}
 
-	glm::vec3 rayOrigin(0.0f, 0.0f, 2.0f);
-	glm::vec3 rayDirection(coord.x, coord.y, -1.0f);
+	m_FinalImage->SetData(m_ImageData);
+}
+
+glm::vec4 Renderer::TraceRay(const Ray& ray)
+{
 	float radius = 0.5f;
-	//rayDirection = glm::normalize(rayDirection);
+	// rayDirection = glm::normalize(rayDirection);
 
-	//  A					B					C
 	// (bx^2 + by^2)t^2 + (2(axbx + ayby))t + (ax^2 + ay^2 - r^2) = 0
-	// Where
+	// where
 	// a = ray origin
 	// b = ray direction
 	// r = radius
-	// t = distance
+	// t = hit distance
 
-	float a = glm::dot(rayDirection, rayDirection);
-	float b = 2.0f * glm::dot(rayOrigin, rayDirection);
-	float c = glm::dot(rayOrigin, rayOrigin) - radius * radius;
+	float a = glm::dot(ray.Direction, ray.Direction);
+	float b = 2.0f * glm::dot(ray.Origin, ray.Direction);
+	float c = glm::dot(ray.Origin, ray.Origin) - radius * radius;
 
-
-	// Quadratic Formula Discriminant
+	// Quadratic forumula discriminant:
 	// b^2 - 4ac
 
-	float discriminant = (b * b - 4.0f * a * c);
-
-	if (discriminant > 0.0f) {
-		float* t = new float[2] {
-			(-b - glm::sqrt(discriminant)) / (2.0f * a),
-			(-b + glm::sqrt(discriminant)) / (2.0f * a)
-		};
+	float discriminant = b * b - 4.0f * a * c;
+	if (discriminant < 0.0f)
+		return glm::vec4(0.05, 0.05, 0.05, 1);
 
 
-		float light;
+	// Quadratic formula:
+	// (-b +- sqrt(discriminant)) / 2a
 
-		for (int i = 0; i < 2; i++) {
-			glm::vec3 hitPosition = rayOrigin + rayDirection * t[i];
-			glm::vec3 normal = hitPosition - glm::vec3(0.0f);
-			normal = glm::normalize(normal);
+	float closestT = (-b - glm::sqrt(discriminant)) / (2.0f * a);
+	float t0 = (-b + glm::sqrt(discriminant)) / (2.0f * a); // Second hit distance (currently unused)
 
-			light = glm::max(glm::dot(normal, -lightDirection), 0.0f);
-		}
+	glm::vec3 hitPoint = ray.Origin + ray.Direction * closestT;
+	glm::vec3 normal = glm::normalize(hitPoint);
+
+	glm::vec3 lightDir = glm::normalize(glm::vec3(-1, -1, -1));
+	float lightIntensity = glm::max(glm::dot(normal, -lightDir), 0.0f); // == cos(angle)
 
 
-		return light * 0xff00ff00;
-	} 
-	return 0xff000000;
+	glm::vec3 sphereColor(1, 0, 1);
+	sphereColor *= lightIntensity;
+	return glm::vec4(sphereColor, 1.0f);
 }
--- WalnutApp/src/Renderer.h
+++ WalnutApp/src/Renderer.h
@@ -2,9 +2,6 @@
 
 #include "Walnut/Image.h"
 
-#include "Camera.h"
-#include "Ray.h"
-
 #include <memory>
 #include <glm/glm.hpp>
 
@@ -14,12 +11,16 @@ public:
 	Renderer() = default;
 
 	void OnResize(uint32_t width, uint32_t height);
-	void Render(const Camera& camera);
+	void Render();
 
 	std::shared_ptr<Walnut::Image> GetFinalImage() const { return m_FinalImage; }
 private:
-	glm::vec4 TraceRay(const Ray& ray);
+	uint32_t PerPixel(glm::vec2 coord);
 private:
 	std::shared_ptr<Walnut::Image> m_FinalImage;
 	uint32_t* m_ImageData = nullptr;
+
+	uint32_t m_SphereColor = 0xff0000ff;
+	uint32_t m_LightColor = 0xffff0000;
+	uint32_t m_BGColor = 0xff000000;
 };
